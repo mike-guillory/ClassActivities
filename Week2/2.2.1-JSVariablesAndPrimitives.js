a7133979b0be9d1858f7d5f3fd93d31a396545ca
Mike Guillory <webdevmike101@gmail.com>
2023-03-28 20:54:39 -0500

    JS Variables and Primitives, Week 2 Class Activity

---
+++ Week2/2.2.1-JSVariablesAndPrimitives.js
@@ -0,0 +1,36 @@
+// create a constant variable (const)
+
+// create a variable that can be reassigned (let)
+
+// create three variables and assign them values of different data types (=)
+
+// print the types of two variables that reference two different data types (typeof)
+// TIP: console.log() prints a value in the console/terminal
+
+// change the value referenced by a variable (dynamic typing)
+
+// print the type of the variable you just changed (typeof)
+
+// create variables and give them values to complete the sentences that will print  below.
+// Which variables need to be created?
+// What type of data needs to go in each variable?
+
+const myConst = "My Constant";
+let myVAr = "My Variable;"
+let myString = "This string";
+let myNumber = 100;
+let myBool = false;
+
+myString = "Another string";
+
+console.log(typeof myString);
+console.log(typeof myBool);
+
+const myName = "Mike";
+let lang;
+let num = 12;
+let tf = true;
+
+lang = "Javascript";
+
+console.log("Hello, my name is " + myName+ ", and I am learning " + lang + ". I have been practicing for only " +  num + " weeks, so my status as a master is: " + tf + ".");
